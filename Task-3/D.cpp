700acd18816d510fcdeee425f33c3c5300ed01dc
menna walid <mennawalid1244@gmail.com>
2026-01-23 16:33:16 +0200

    Create D.cpp

---
+++ Task-3/D.cpp
@@ -0,0 +1,103 @@
+/*
+                          بِسْمِ اللَّـهِ الرَّحْمَـ ٰنِ الرَّحِيمِ 
+*/
+#include<iostream>
+using namespace std;
+#include <bits/stdc++.h>
+#define int long long
+#define ll long long
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define endl '\n'
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define popCnt(x) (__builtin_popcountll(x))
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+#define By_menna ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+void UseFile(){freopen("ProblemName.in", "r", stdin);}
+/*
+int n;
+cin>>n;
+vector<int>v(n);
+ for(int i=0;i<n;i++){
+  cin>>v[i];  }
+  */ 
+/*
+
+*/
+const int dx[]={0,0,1,-1};
+const int dy[]={1,-1,0,0};
+const int MOD = 1e3 + 7;
+const ll INF = 1e18;
+const int N = 1e5 + 5;
+#define pii pair<int,int>
+int safe_sqrt(int n) {
+    if (n <= 0) return 0;
+    int res = sqrtl(n);
+   
+    while ((res + 1) * (res + 1) <= n) res++;
+    while (res * res > n) res--;
+    return res;
+}
+int LIS(vector<int>& a) {
+    int n = a.size();
+    vector<int> dp(n, 1);
+    int ans = 1;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < i; j++) {
+            if(a[j] < a[i]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+        ans = max(ans, dp[i]);
+    }
+    return ans;
+}
+bool can(int mid,vector<int>&v,int t){
+    //with the time mid how many products i can make???
+    int num=0;
+    for(int i=0;i<v.size();i++){
+        num+=mid/v[i];
+        if (num >= t) return true; 
+    }
+    return num>=t;
+}
+void Yarab_ACC(){
+  /*
+   int t;
+cin>>t;
+while(t--){
+}
+  */ 
+ 
+int n,t;
+cin>>n>>t;
+vector<int>v(n);
+ for(int i=0;i<n;i++){
+  cin>>v[i];  }
+  int l=0,r=1e18;
+  int ans=-1;
+  while(l<=r){
+    int mid=(l+r)/2;
+    if(can(mid,v,t)){
+        ans=mid;
+        //miniii
+         r=mid-1;
+
+    }
+    else{l=mid+1;
+       
+    }
+  }
+
+cout<<ans<<endl;
+}
+signed main() {
+  By_menna;
+    //UseFile();
+ Yarab_ACC();
+    return 0;
+}
